87ff713ac54bbd59dab761f95db0c1ea95913a24
Joe Chance <joe.chance@hotmail.com>
2023-01-25 15:38:24 +1100

    Refactor tests

--- identity/identity_test.go
+++ identity/identity_test.go
@@ -16,67 +16,54 @@ type TestIdentityResponse struct {
 	Role string `json:"role"`
 }
 
-func TestIdentityUser(t *testing.T) {
+func testHandler(t *testing.T, userID *string, expectedStatus int, expectedBody *TestIdentityResponse) {
 	writer := httptest.NewRecorder()
 	req, err := http.NewRequest("GET", "/me", nil)
-	req.Header.Add("authorization", "1")
+	if userID != nil {
+		req.Header.Add("authorization", *userID)
+	}
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	identity.Handler(writer, req)
 
-	assert.Equal(t, http.StatusOK, writer.Result().StatusCode)
+	assert.Equal(t, expectedStatus, writer.Result().StatusCode)
 	resBody := TestIdentityResponse{}
+	if expectedBody == nil {
+		return
+	}
 	if err = json.Unmarshal(writer.Body.Bytes(), &resBody); err != nil {
 		t.Fatalf("Failed to unmarshal response: %s", err)
 	}
-	assert.Equal(t, "1", resBody.ID)
-	assert.Equal(t, "John Smith", resBody.Name)
-	assert.Equal(t, "user", resBody.Role)
+	assert.Equal(t, expectedBody.ID, resBody.ID)
+	assert.Equal(t, expectedBody.Name, resBody.Name)
+	assert.Equal(t, expectedBody.Role, resBody.Role)
+}
+
+func TestIdentityUser(t *testing.T) {
+	userID := "1"
+	testHandler(t, &userID, 200, &TestIdentityResponse{
+		ID:   "1",
+		Name: "John Smith",
+		Role: "user",
+	})
 }
 
 func TestIdentityAdmin(t *testing.T) {
-	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/me", nil)
-	req.Header.Add("authorization", "2")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	identity.Handler(writer, req)
-
-	assert.Equal(t, http.StatusOK, writer.Result().StatusCode)
-	resBody := TestIdentityResponse{}
-	if err = json.Unmarshal(writer.Body.Bytes(), &resBody); err != nil {
-		t.Fatalf("Failed to unmarshal response: %s", err)
-	}
-	assert.Equal(t, "2", resBody.ID)
-	assert.Equal(t, "Bob Bloggs", resBody.Name)
-	assert.Equal(t, "admin", resBody.Role)
+	userID := "2"
+	testHandler(t, &userID, 200, &TestIdentityResponse{
+		ID:   "2",
+		Name: "Bob Bloggs",
+		Role: "admin",
+	})
 }
 
 func TestIdentityUnauthenticated(t *testing.T) {
-	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/me", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	identity.Handler(writer, req)
-
-	assert.Equal(t, http.StatusUnauthorized, writer.Result().StatusCode)
+	testHandler(t, nil, http.StatusUnauthorized, nil)
 }
 
 func TestIdentityNotExisting(t *testing.T) {
-	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/me", nil)
-	req.Header.Add("authorization", "1001")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	identity.Handler(writer, req)
-
-	assert.Equal(t, http.StatusNotFound, writer.Result().StatusCode)
+	userID := "1001"
+	testHandler(t, &userID, http.StatusNotFound, nil)
 }
