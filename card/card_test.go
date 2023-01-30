c8489b5fc92324e31d7df36f5b833fe25d276f90
Joe Chance <joe.chance@hotmail.com>
2023-01-30 19:08:24 +1100

    Refactor /card tests

--- card/card_test.go
+++ card/card_test.go
@@ -20,10 +20,12 @@ type TestCardResponse struct {
 	Balance      int    `json:"balance"`
 }
 
-func TestCardUser(t *testing.T) {
+func testGetHandler(t *testing.T, authUser *string, expectedStatus int, expectedBody *TestCardResponse) {
 	writer := httptest.NewRecorder()
 	req, err := http.NewRequest("GET", "/card", nil)
-	req.Header.Add("authorization", "1")
+	if authUser != nil {
+		req.Header.Add("authorization", *authUser)
+	}
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -40,46 +42,37 @@ func TestCardUser(t *testing.T) {
 		},
 	)).ServeHTTP(writer, req)
 
-	assert.Equal(t, http.StatusOK, writer.Result().StatusCode)
+	assert.Equal(t, expectedStatus, writer.Result().StatusCode)
+	if expectedBody == nil {
+		return
+	}
 	resBody := TestCardResponse{}
 	if err = json.Unmarshal(writer.Body.Bytes(), &resBody); err != nil {
 		t.Fatalf("Failed to unmarshal response: %s", err)
 	}
-	assert.Equal(t, "**** **** **** 1111", resBody.MaskedNumber)
-	assert.Equal(t, "12/23", resBody.Exp)
-	assert.Equal(t, 10000, resBody.Limit)
-	assert.Equal(t, 4321, resBody.Balance)
+	assert.Equal(t, expectedBody.MaskedNumber, resBody.MaskedNumber)
+	assert.Equal(t, expectedBody.Exp, resBody.Exp)
+	assert.Equal(t, expectedBody.Limit, resBody.Limit)
+	assert.Equal(t, expectedBody.Balance, resBody.Balance)
+}
+
+func TestCardUser(t *testing.T) {
+	userID := "1"
+	testGetHandler(t, &userID, http.StatusOK, &TestCardResponse{
+		MaskedNumber: "**** **** **** 1111",
+		Exp:          "12/23",
+		Limit:        10000,
+		Balance:      4321,
+	})
 }
 
 func TestCardUnauthorized(t *testing.T) {
-	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/card", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	card.NewHandler(repo.NewRepo(
-		map[string]repo.User{},
-		map[string]repo.Card{},
-	)).ServeHTTP(writer, req)
-
-	assert.Equal(t, http.StatusUnauthorized, writer.Result().StatusCode)
+	testGetHandler(t, nil, http.StatusUnauthorized, nil)
 }
 
 func TestNoCardForUser(t *testing.T) {
-	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/card", nil)
-	req.Header.Add("authorization", "1")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	card.NewHandler(repo.NewRepo(
-		map[string]repo.User{},
-		map[string]repo.Card{},
-	)).ServeHTTP(writer, req)
-
-	assert.Equal(t, http.StatusNotFound, writer.Result().StatusCode)
+	userID := "1001"
+	testGetHandler(t, &userID, http.StatusNotFound, nil)
 }
 
 type TestLimitRequest struct {
@@ -87,18 +80,16 @@ type TestLimitRequest struct {
 	NewLimit int
 }
 
-func TestUserShouldNotBeAbleToModifyLimit(t *testing.T) {
+func testChangeLimitAPI(t *testing.T, reqB *TestLimitRequest, authUser *string, expectedStatus int) {
 	writer := httptest.NewRecorder()
-	reqB := TestLimitRequest{
-		UserID:   "1",
-		NewLimit: 20000,
-	}
 	jBody, err := json.Marshal(reqB)
 	if err != nil {
 		t.Fatalf("Error marshalling request body: %v", err)
 	}
 	req, err := http.NewRequest("POST", "/card", bytes.NewBuffer(jBody))
-	req.Header.Add("authorization", "1")
+	if authUser != nil {
+		req.Header.Add("authorization", *authUser)
+	}
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -126,55 +117,21 @@ func TestUserShouldNotBeAbleToModifyLimit(t *testing.T) {
 		},
 	)).ServeHTTP(writer, req)
 
-	assert.Equal(t, http.StatusForbidden, writer.Result().StatusCode)
+	assert.Equal(t, expectedStatus, writer.Result().StatusCode)
+}
+
+func TestUserShouldNotBeAbleToModifyLimit(t *testing.T) {
+	userID := "1"
+	testChangeLimitAPI(t, &TestLimitRequest{
+		UserID:   "1",
+		NewLimit: 20000,
+	}, &userID, http.StatusForbidden)
 }
 
 func TestAdminShouldBeAbleToModifyLimit(t *testing.T) {
-	writer := httptest.NewRecorder()
-	reqB := TestLimitRequest{
+	userID := "2"
+	testChangeLimitAPI(t, &TestLimitRequest{
 		UserID:   "1",
 		NewLimit: 20000,
-	}
-	jBody, err := json.Marshal(reqB)
-	if err != nil {
-		t.Fatalf("Error marshalling request body: %v", err)
-	}
-	req, err := http.NewRequest("POST", "/card", bytes.NewBuffer(jBody))
-	req.Header.Add("authorization", "2")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	card.NewHandler(repo.NewRepo(
-		map[string]repo.User{
-			"1": {
-				ID:   "1",
-				Name: "John Reece",
-				Role: "user",
-			},
-			"2": {
-				ID:   "2",
-				Name: "Bob Smith",
-				Role: "admin",
-			},
-		},
-		map[string]repo.Card{
-			"1": {
-				MaskedNumber: "**** **** **** 1111",
-				Exp:          "12/23",
-				Limit:        10000,
-				Balance:      4321,
-			},
-		},
-	)).ServeHTTP(writer, req)
-
-	assert.Equal(t, http.StatusOK, writer.Result().StatusCode)
-	resBody := TestCardResponse{}
-	if err = json.Unmarshal(writer.Body.Bytes(), &resBody); err != nil {
-		t.Fatalf("Failed to unmarshal response: %s", err)
-	}
-	assert.Equal(t, "**** **** **** 1111", resBody.MaskedNumber)
-	assert.Equal(t, "12/23", resBody.Exp)
-	assert.Equal(t, 20000, resBody.Limit)
-	assert.Equal(t, 4321, resBody.Balance)
+	}, &userID, http.StatusOK)
 }
